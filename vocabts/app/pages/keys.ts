e41ec5791d6711c256143409e7f1f82066765723
Ahmet Anıl GÜR <ahmetanilgur@gmail.com>
2016-03-11 18:23:49 +0200

    vocabts transferred

---
+++ vocabts/app/pages/keys.ts
@@ -0,0 +1,19 @@
+module Shapes {
+    export module Polygons {
+        export class Triangle {
+          keywords=[
+     'Running: Çalışan',
+     'Host: Ana makine',
+     'Unauthorized: Yetkisiz',
+     'Vulnerability: Zafiyet',
+     'Homepage: Anasayfa',
+     'Dictionary: Sözlük',
+     'DoS: Denial-of-Service',
+     'Device: Aygıt'
+     
+    ];
+         }
+
+    }
+}
+
